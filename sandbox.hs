8a24065177138dec68cb83c934ca9d6e51a7f5e3
John Li <johnli0135@gmail.com>
2019-08-13 14:29:42 -0700

    Cleanup

--- sandbox.hs
+++ sandbox.hs
@@ -56,13 +56,13 @@ snd (pair _ Y) = Y.
 lo : bool.
 hi : bool.
 
-N : bnat <== N : (list bool).
-
 normalized0 (hi :: nil).
 normalized0 (lo :: N) <== normalized0 N.
 normalized0 (hi :: (L :: N)) <== normalized0 (L :: N).
 normalized nil.
 normalized (L :: N) <== normalized0 (L :: N).
+
+N : bnat <== N : (list bool), normalized N.
 
 and lo _ = lo.
 and _ lo = lo.
@@ -81,21 +81,21 @@ suc nil = (hi :: nil).
 suc (lo :: N) = (hi :: N).
 suc (hi :: N) = (lo :: M) where suc N = M.
 
-adc lo lo lo => (lo lo).
-adc lo lo hi => (hi lo).
-adc lo hi lo => (hi lo).
-adc lo hi hi => (lo hi).
-adc hi lo lo => (hi lo).
-adc hi lo hi => (lo hi).
-adc hi hi lo => (lo hi).
-adc hi hi hi => (hi hi).
+adc lo lo lo => lo lo.
+adc lo lo hi => hi lo.
+adc lo hi lo => hi lo.
+adc lo hi hi => lo hi.
+adc hi lo lo => hi lo.
+adc hi lo hi => lo hi.
+adc hi hi lo => lo hi.
+adc hi hi hi => hi hi.
 
 adc nil N lo = N.
 adc nil N hi = M where suc N = M.
 adc N nil lo = N.
 adc N nil hi = M where suc N = M.
 adc (L :: M) (R :: N) C = (LR :: P) where
-  adc L R C => (LR C1),
+  adc L R C => LR C1,
   adc M N C1 = P.
 
 N + M = P where adc N M lo = P.
@@ -104,36 +104,40 @@ nil * _ = nil.
 (lo :: M) * N = (lo :: P) where M * N = P.
 (hi :: M) * N = P where M * N = K, N + (lo :: K) = P.
 
--- -- 11 * 23 = ? (253)
+-- -- Binary multiplication: 11 * 23 = ?(253)
 -- 10 M N P ?
 --   let M = (hi :: (hi :: (lo :: (hi :: nil)))),
 --   let N = (hi :: (hi :: (hi :: (lo :: (hi :: nil))))),
 --   M * N = P
 
--- -- 11 * ? (23) = 253
+-- -- Binary division: 11 * ?(23) = 253
 -- 9 M N P ?
 --   let M = (hi :: (hi :: (lo :: (hi :: nil)))),
 --   let P = (hi :: (lo :: (hi :: (hi :: (hi :: (hi :: (hi :: (hi :: nil)))))))),
 --   M * N = P
 
--- -- Type inference
--- 9 XS YS ZS T ?
---   let XS = (hi :: (lo :: (hi :: (hi :: (hi :: (hi :: (hi :: (hi :: nil)))))))),
---   let YS = (hi :: (hi :: (lo :: (hi :: nil)))),
---   zipw pair XS YS = ZS,
---   ZS : T
+-- Type inference
+9 XS YS ZS T ?
+  let XS = (hi :: (lo :: (hi :: (hi :: (hi :: (hi :: (hi :: (hi :: nil)))))))),
+  let YS = (hi :: (hi :: (lo :: (hi :: nil)))),
+  zipw pair XS YS = ZS,
+  ZS : T
 
+-- -- Binary subtraction: 10 + ?(7) = 17
 -- 10 M N P ?
 --   let M = (lo :: (hi :: (lo :: (hi :: nil)))),
 --   let P = (hi :: (lo :: (lo :: (lo :: (hi :: nil))))),
 --   add M N = P,
 --   normalized N
 
+-- -- List concatenation: [x, y, z] ++ [u, v] = ?(x) :: y :: ?([z, u, v])
 -- 3 X ZUV ? (x :: (y :: (z :: nil))) ++ (u :: (v :: nil)) = (X :: (y :: ZUV))
+
+-- -- Unary multiplication: 3 * 4 = ?(12)
 -- 10 X ? (s (s (s z))) * (s (s (s (s z)))) = X
+
+-- -- Function synthesis: map ?((2 +)) [0, 1, 2, 3] = [2, 3, 4, 5]
 -- 10 F ? map F (z :: ((s z) :: ((s (s z)) :: ((s (s (s z))) :: nil)))) = ((s (s z)) :: ((s (s (s z))) :: ((s (s (s (s z)))) :: ((s (s (s (s (s z))))) :: nil))))
 
+-- -- Zipwith: zipWith (+) [0, 1, 2, 3] [2, 3, 4, 5] = ?([(0, 2), (1, 3), (2, 4), (3, 5)])
 -- 10 XS ? (zipw add (z :: ((s z) :: ((s (s z)) :: ((s (s (s z))) :: nil)))) ((s (s z)) :: ((s (s (s z))) :: ((s (s (s (s z)))) :: ((s (s (s (s (s z))))) :: nil))))) = XS
-
--- 10 N ? add (lo :: (hi :: (lo :: (hi :: nil)))) (hi :: (hi :: (hi :: nil))) = N
--- 10 M N ? add M N = (hi :: (lo :: (lo :: (lo :: (hi :: nil)))))
